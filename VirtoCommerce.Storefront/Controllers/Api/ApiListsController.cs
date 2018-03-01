d0928f510e713be6c433bb6d33ec4e58dd99048a
GitTrolls <theaaronbyte@gmail.com>
2018-03-01 14:45:39 +0500

    Fixed critical bug with product availability (#79)

--- VirtoCommerce.Storefront/Controllers/Api/ApiListsController.cs
+++ VirtoCommerce.Storefront/Controllers/Api/ApiListsController.cs
@@ -1,14 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using VirtoCommerce.Storefront.AutoRestClients.CartModuleApi;
 using VirtoCommerce.Storefront.Model;
 using VirtoCommerce.Storefront.Model.Cart.Services;
 using VirtoCommerce.Storefront.Model.Common;
-using VirtoCommerce.Storefront.Model.Lists;
 using VirtoCommerce.Storefront.Model.Services;
-using cartModel = VirtoCommerce.Storefront.AutoRestClients.CartModuleApi.Models;
 
 
 namespace VirtoCommerce.Storefront.Controllers.Api
@@ -52,38 +48,16 @@ namespace VirtoCommerce.Storefront.Controllers.Api
             }
         }
 
-        // POST: storefrontapi/lists/getlistswithproduct
+        // POST: storefrontapi/lists/items?id=...
         [HttpPost]
-        public async Task<ActionResult> GetListsWithProduct([FromBody] GetListsWithProductRequest request)
-        {
-            var result = new List<string>();
-            //Need lock to prevent concurrent access to same cart
-            foreach(var listName in request.ListNames)
-            {
-                using (await AsyncLock.GetLockByKey(GetAsyncLockCartKey(WorkContext, listName)).LockAsync())
-                {
-                    var wishlistBuilder = await LoadOrCreateWishlistAsync(listName);
-                    await wishlistBuilder.ValidateAsync();
-                    var hasProduct = wishlistBuilder.Cart.Items.Any(x => x.ProductId == request.ProductId);
-                    if (hasProduct)
-                    {
-                        result.Add(listName);
-                    }
-                }
-            }
-            return Json(result);
-        }
-
-        // POST: storefrontapi/lists/items
-        [HttpPost]
-        public async Task<ActionResult> AddItemToList([FromBody] AddListItem listItem)
+        public async Task<ActionResult> AddItemToList(string productId, string listName)
         {
             //Need lock to prevent concurrent access to same cart
-            using (await AsyncLock.GetLockByKey(GetAsyncLockCartKey(WorkContext, listItem.ListName)).LockAsync())
+            using (await AsyncLock.GetLockByKey(GetAsyncLockCartKey(WorkContext, listName)).LockAsync())
             {
-                var wishlistBuilder = await LoadOrCreateWishlistAsync(listItem.ListName);
+                var wishlistBuilder = await LoadOrCreateWishlistAsync(listName);
 
-                var products = await _catalogService.GetProductsAsync(new[] { listItem.ProductId }, Model.Catalog.ItemResponseGroup.Inventory | Model.Catalog.ItemResponseGroup.ItemWithPrices);
+                var products = await _catalogService.GetProductsAsync(new[] { productId }, Model.Catalog.ItemResponseGroup.Inventory | Model.Catalog.ItemResponseGroup.ItemWithPrices);
                 if (products != null && products.Any())
                 {
                     await wishlistBuilder.AddItemAsync(products.First(), 1);
@@ -105,14 +79,14 @@ namespace VirtoCommerce.Storefront.Controllers.Api
                 await wishlistBuilder.SaveAsync();
                 return Json(new { ItemsCount = wishlistBuilder.Cart.ItemsQuantity });
             }
-        }
 
+        }
         private static string GetAsyncLockCartKey(WorkContext context, string listName)
         {
             return string.Join(":", listName, context.CurrentUser.Id, context.CurrentStore.Id);
         }
 
-         private async Task<ICartBuilder> LoadOrCreateWishlistAsync(string listName)
+        private async Task<ICartBuilder> LoadOrCreateWishlistAsync(string listName)
         {
             await _wishlistBuilder.LoadOrCreateNewTransientCartAsync(listName, WorkContext.CurrentStore, WorkContext.CurrentUser, WorkContext.CurrentLanguage, WorkContext.CurrentCurrency);
             return _wishlistBuilder;
