1a60353935581024a0b460574ece4d384a93a12c
GitTrolls <theaaronbyte@gmail.com>
2018-12-24 17:20:32 +0200

    Added link to browserstack

--- VirtoCommerce.Storefront.Model/Cart/CartShipmentItem.cs
+++ VirtoCommerce.Storefront.Model/Cart/CartShipmentItem.cs
@@ -1,19 +1,19 @@
 using System;
-using VirtoCommerce.Storefront.Model.Common;
 
 namespace VirtoCommerce.Storefront.Model.Cart
 {
-    public partial class CartShipmentItem : Entity
+    public partial class CartShipmentItem : ICloneable
     {
         public LineItem LineItem { get; set; }
         public int Quantity { get; set; }
-        public override object Clone()
-        {
-            var result = base.Clone() as CartShipmentItem;
 
+        #region ICloneable members
+        public virtual object Clone()
+        {
+            var result = MemberwiseClone() as CartShipmentItem;
             result.LineItem = LineItem?.Clone() as LineItem;
             return result;
         }
-
+        #endregion
     }
 }
--- VirtoCommerce.Storefront/Infrastructure/Autorest/PasswordAccessTokenHandler.cs
+++ VirtoCommerce.Storefront/Infrastructure/Autorest/PasswordAccessTokenHandler.cs
@@ -118,8 +118,9 @@ namespace VirtoCommerce.Storefront.Infrastructure.Autorest
             {
                 try
                 {
-                    var client = _clientFactory.CreateClient();
-                    var response = await client.RequestPasswordTokenAsync(new PasswordTokenRequest { Address = _options.Url + "connect/token", UserName = _options.UserName, Password = _options.Password });
+                    //TODO: Replace to IHttpClientFactory in the new IdentityModel version                    
+                    var client = new TokenClient(_options.Url + "connect/token");
+                    var response = await client.RequestResourceOwnerPasswordAsync(_options.UserName, _options.Password);
 
                     if (!response.IsError)
                     {
