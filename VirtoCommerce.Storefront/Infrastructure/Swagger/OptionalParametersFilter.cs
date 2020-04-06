c8de6658e95e933b90e4a2a79b0b516dc01d35c9
GitTrolls <theaaronbyte@gmail.com>
2020-04-06 15:21:46 +0500

    4.1.10

--- VirtoCommerce.Storefront/Infrastructure/Swagger/OptionalParametersFilter.cs
+++ VirtoCommerce.Storefront/Infrastructure/Swagger/OptionalParametersFilter.cs
@@ -1,13 +1,13 @@
 using System.Linq;
 using Microsoft.AspNetCore.Mvc.Controllers;
-using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace VirtoCommerce.Storefront.Infrastructure.Swagger
 {
     public class OptionalParametersFilter : IOperationFilter
     {
-        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        public void Apply(Operation operation, OperationFilterContext context)
         {
             if (operation.Parameters?.Any() == true)
             {
